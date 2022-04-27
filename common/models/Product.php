019ad1bb92de9160ee24bb89098c57c23ab3c977
Jake <zhandos38@gmail.com>
2022-04-27 14:47:29 +0600

    fixes

--- common/models/Product.php
+++ common/models/Product.php
@@ -20,6 +20,7 @@ class Product extends BaseProduct
             [['name_ru', 'name_kz', 'name_en', 'name_ch'], 'string', 'max' => 255],
             [['short_description_ru', 'short_description_kz', 'short_description_en', 'short_description_ch'], 'string'],
             [['description_ru', 'description_kz', 'description_en', 'description_ch'], 'string'],
+            [['price', 'price_old', 'quantity'], 'number'],
 
             [['category_id', 'created_at', 'updated_at'], 'integer'],
 
